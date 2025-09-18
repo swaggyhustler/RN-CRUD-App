abd0344428949ad2d7170fa530032845f0e077a4
swaggyhustler <gedamsubhash086@gmail.com>
2025-09-18 18:09:32 +0530

    Add Google Font

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Index from './src/index.tsx';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <Index />
+    <SafeAreaProvider>
+        <Index />
+    </SafeAreaProvider>
   );
 }
 
---
+++ constants/Colors.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+    light: {
+        text:'black',
+        background: '#fff',
+        icon: 'black',
+        button: 'royalBlue'
+    },
+    dark: {
+        text:'white',
+        background: '#000',
+        icon: 'red',
+        button: 'white'
+    }
+}
